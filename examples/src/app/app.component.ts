32202579ca24186f42965d29a2f46badedd05bf9
ukyo <ukyo.web@gmail.com>
2017-07-04 18:08:57 +0900

    fix title

--- examples/src/app/app.component.ts
+++ examples/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Learn Reactive Form';
+  title = 'Learn Reactive Forms';
 }
