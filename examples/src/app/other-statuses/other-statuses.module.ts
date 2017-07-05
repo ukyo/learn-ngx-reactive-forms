0eb018ad122def6755d919499e9e99dcfceeb153
ukyo <ukyo.web@gmail.com>
2017-07-05 13:34:55 +0900

    scaffold

---
+++ examples/src/app/other-statuses/other-statuses.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OtherStatusesRoutingModule } from './other-statuses-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    OtherStatusesRoutingModule
+  ],
+  declarations: []
+})
+export class OtherStatusesModule { }
