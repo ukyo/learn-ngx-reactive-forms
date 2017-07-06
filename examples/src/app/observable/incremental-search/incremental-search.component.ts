21f18054548787c253fa996c81fb0b3b7c2e2803
ukyo <ukyo.web@gmail.com>
2017-07-06 20:54:16 +0900

    update

--- examples/src/app/observable/incremental-search/incremental-search.component.ts
+++ examples/src/app/observable/incremental-search/incremental-search.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+const list = [
+  'apple',
+  'banana',
+  'dog',
+  'big dog',
+  'small dog',
+  'cat',
+  'big cat',
+  'application',
+];
 
 @Component({
   selector: 'incremental-search',
   templateUrl: './incremental-search.component.html',
   styleUrls: ['./incremental-search.component.css']
 })
-export class IncrementalSearchComponent implements OnInit {
+export class IncrementalSearchComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  formGroup: FormGroup;
+  sub = new Subscription();
+  results = [];
+
+  constructor(fb: FormBuilder) {
+    this.formGroup = fb.group({ query: '' });
+    this.sub.add(
+      this.formGroup.get('query')
+      .valueChanges
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .subscribe(q => {
+        if (!q) {
+          this.results = [];
+          return;
+        }
+        this.results = list.filter(s => s.includes(q));
+      })
+    );
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
--- examples/src/app/observable/update-value-and-status-manually/update-value-and-status-manually.component.ts
+++ examples/src/app/observable/update-value-and-status-manually/update-value-and-status-manually.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'update-value-and-status-manually',
   templateUrl: './update-value-and-status-manually.component.html',
   styleUrls: ['./update-value-and-status-manually.component.css']
 })
-export class UpdateValueAndStatusManuallyComponent implements OnInit {
+export class UpdateValueAndStatusManuallyComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  formGroup: FormGroup;
+  sub = new Subscription;
+
+  constructor(fb: FormBuilder) {
+    this.formGroup = fb.group({
+      name: ['', Validators.required]
+    });
+    this.sub.add(this.formGroup.valueChanges.subscribe(v => console.log(v)));
+    this.sub.add(this.formGroup.statusChanges.subscribe(s => console.log(s)));
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
