316669bf56d7872d81a97a227d270453d74bbdc9
ukyo <ukyo.web@gmail.com>
2017-07-06 13:51:44 +0900

    add touched and dirty

--- examples/src/app/other-statuses/touched-and-dirty/touched-and-dirty.component.ts
+++ examples/src/app/other-statuses/touched-and-dirty/touched-and-dirty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoginFormGroupFactoryService } from 'app/intro/login-form-group-factory.service';
 
 @Component({
   selector: 'touched-and-dirty',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TouchedAndDirtyComponent implements OnInit {
 
-  constructor() { }
+  formGroup1: FormGroup;
+  formGroup2: FormGroup;
+
+  constructor(loginFormGroupFactory: LoginFormGroupFactoryService) {
+    this.formGroup1 = loginFormGroupFactory.create({
+      name: '',
+      password: '',
+    });
+    this.formGroup2 = loginFormGroupFactory.create({
+      name: '',
+      password: '',
+    });
+  }
 
   ngOnInit() {
   }
